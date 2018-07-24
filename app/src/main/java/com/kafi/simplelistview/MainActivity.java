0a0bc31b4888b895a41b1440fd8897c1c977aa69
Shamim kafi <shamimunbindelowarkafi@gmail.com>
2018-07-24 14:08:51 +0600

    Initial commit

---
+++ app/src/main/java/com/kafi/simplelistview/MainActivity.java
@@ -0,0 +1,36 @@
+package com.kafi.simplelistview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    String worldCup[] ={"Argentina","Belgium","Spain","France"};
+    ListView listView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        listView = findViewById(R.id.listViewId);
+
+        ArrayAdapter adapter = new ArrayAdapter(this,R.layout.support_simple_spinner_dropdown_item,worldCup);
+        listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+                Toast.makeText(MainActivity.this, worldCup[i], Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+    }
+}
